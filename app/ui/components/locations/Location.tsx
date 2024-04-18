d49720989afefed3ab444f884013820672aed034
rytwalker <rytwalker@gmail.com>
2024-04-18 15:31:19 -0400

    more formatting

--- app/ui/components/locations/Location.tsx
+++ app/ui/components/locations/Location.tsx
@@ -3,12 +3,9 @@ import { Location as LocationType } from "@/app/lib/definitions";
 const Location = ({ location }: { location: LocationType }) => {
   return (
     <li key={location.name} className="uppercase text-xs mb-4">
+      <p> {location.name}</p>
+      <p>{location.address}</p>
       <p>
-        {" "}
-        {location.name} -{location.address}{" "}
-      </p>
-      <p>
-        {" "}
         <a className="text-kagu-green-500 underline" href="https://google.com">
           website
         </a>{" "}
