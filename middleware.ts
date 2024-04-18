ec101eb62c03d796b0afc1dce17ea00231b367ca
rytwalker <rytwalker@gmail.com>
2024-04-18 11:56:05 -0400

    Delete middleware.ts

--- middleware.ts
+++
@@ -1,6 +0,0 @@
-// import { NextRequest } from "next/server";
-// import { updateSession } from "./app/lib/auth";
-//
-// export async function middleware(request: NextRequest) {
-//   return await updateSession(request);
-// }
